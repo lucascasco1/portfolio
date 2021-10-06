99fff0e825c0c78056ae2392745d7515e68d9c52
Lucas Casco <thunderstruck.lc@gmail.com>
2021-10-06 20:48:53 -0300

    carpeta de prueba

---
+++ prueba/script.js
@@ -0,0 +1,80 @@
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+        
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+        
+//         // mostrar nav segun scroll
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
+
+//     // animacion hacia arriba
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         $('html').css("scrollBehavior", "auto");
+//     });
+
+//     $('.navbar .menu li a').click(function(){
+//         $('html').css("scrollBehavior", "smooth");
+//     });
+
+//     // navbar/menu (desktop/mobile)
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+
+//     // cambiar texto
+//     var typed = new Typed(".typing", {
+//         strings: ["Front end developer", "Back end developer", "Full stack developer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     var typed = new Typed(".typing-2", {
+//         strings: ["Front end developer", "Back end developer", "Full stack developer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     // carrusel
+//     $('.carousel').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         autoplayTimeOut: 1000,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0:{
+//                 items: 1,
+//                 nav: false
+//             },
+//             600:{
+//                 items: 2,
+//                 nav: false
+//             },
+//             1000:{
+//                 items: 3,
+//                 nav: false
+//             }
+//         }
+//     });
+
+//     //scrollBar
+
+//     let progress = document.getElementById('progressBar');
+//     let totalHeight = document.body.scrollHeight - window.innerHeight;
+//     window.onscroll = function(){
+//         let progressHeight = (window.pageYOffset / totalHeight) * 100;
+//         progress. style.height = progressHeight + "%";
+//     }
+// });
